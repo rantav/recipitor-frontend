161d405aa30d53b0e5da6f81da1984d941650b19
Ran Tavory <rantav@gmail.com>
2011-02-09 13:22:26 +0200

    Add a barebones default rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipitorFrontend::Application.config.secret_token = '16df510d6cf275b4cc6959494e812cc0aec1188ad234b667b96933721f57368dd1cf5a3cbe6c478943a965d7dba4bb14c373da8f06dd900d18ac4987ade81cb0'
