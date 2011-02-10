c77e202bc117cc7e1ce3651e65dc7ead18ef4ebe
Ran Tavory <rantav@gmail.com>
2011-02-10 23:49:54 +0200

    [#9754851] Make image ACL private

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,8 +1,9 @@
 class Receipt < ActiveRecord::Base
   has_attached_file :img, 
-    :styles => { :thumb => "100x100#" },
+    :styles => {:thumb => "100x100#"},
     :storage => PAPERCLIP_STORAGE_MECHANISM,
     :s3_credentials => Rails.root.join('config/s3.yml'),
+    :s3_permissions => 'private',
     :path => PAPERCLIP_PATH
 
   validates_attachment_presence :img
