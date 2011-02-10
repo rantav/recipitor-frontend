0364957327b975f6933cb479a18d9b1fb5c45f51
Ran Tavory <rantav@gmail.com>
2011-02-10 15:17:34 +0200

    [#9754851] Fix warnine, use Rails.root instead of  #{RAILS_ROOT}

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,7 +2,7 @@ class Receipt < ActiveRecord::Base
   has_attached_file :img, 
     :styles => { :thumb => "100x100#" },
     :storage => PAPERCLIP_STORAGE_MECHANISM,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => Rails.root.join('config/s3.yml'),
     :path => PAPERCLIP_PATH
 
   validates_attachment_presence :img
