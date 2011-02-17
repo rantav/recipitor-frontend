4e1da63d0431bbbfdf436f15ba8285e69288bcc3
Ran Tavory <rantav@gmail.com>
2011-02-17 15:31:39 +0200

    Remove residue from an old schema. cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,6 @@ ActiveRecord::Schema.define(:version => 20110217072448) do
   create_table "receipts", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "img_orig_file_name"
-    t.string   "img_orig_content_type"
-    t.integer  "img_orig_file_size"
-    t.datetime "img_orig_updated_at"
     t.string   "img_file_name"
     t.string   "img_content_type"
     t.integer  "img_file_size"
