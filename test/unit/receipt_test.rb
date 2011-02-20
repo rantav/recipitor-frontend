ba555161642eec7cf0e5a03c073eacdaf6063b04
Ran Tavory <rantav@gmail.com>
2011-02-20 11:44:51 +0200

    Add a bunch of unit tests for the receipts tests for image types uploads

--- test/unit/receipt_test.rb
+++ test/unit/receipt_test.rb
@@ -1,8 +1,51 @@
 require 'test_helper'
 
 class ReceiptTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "should not save receipt without attachment" do
+    receipt = Receipt.new
+    assert !receipt.save
   end
+  
+  test "should save receipt with attachment" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt0.gif', 'image/gif'))
+    assert receipt.save
+  end
+
+  test "attachment content type image/gif should be ok" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt0.gif', 'image/gif'))
+    assert receipt.save
+  end
+  
+  test "attachment content type image/jpg should be ok" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt1.jpg', 'image/jpg'))
+    assert receipt.save
+  end
+  
+  test "attachment content type image/jpeg should be ok" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt3.jpeg', 'image/jpeg'))
+    assert receipt.save
+  end
+
+  test "attachment content type image/bmp should be ok" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt4.bmp', 'image/bmp'))
+    assert receipt.save
+  end
+  
+  test "attachment content type image/pmg should be ok" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt5.png', 'image/png'))
+    assert receipt.save
+  end
+
+  test "attachment content type text/plain should fail" do
+    receipt = Receipt.create(:img => fixture_file_upload('files/receipt6.txt', 'text/plain'))
+    assert !receipt.save
+  end
+
+
+  ## This test runs for too long... (57 seconds...)
+#  test "attachment too big should be rejected" do
+#    receipt = Receipt.create(:img => fixture_file_upload('files/large_img.jpg', 'image/jpg'))
+#    assert !receipt.save
+#  end
 end
