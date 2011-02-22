ef8e502d158ffa9c255802a8128cd3300069081b
Ran Tavory <rantav@gmail.com>
2011-02-22 15:58:27 +0200

    Make receipts 10 per page

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -2,7 +2,7 @@ class ReceiptsController < ApplicationController
   # GET /receipts
   # GET /receipts.xml
   def index
-    @receipts =  Receipt.paginate :page => params[:page], :per_page => 5
+    @receipts =  Receipt.paginate :page => params[:page], :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
