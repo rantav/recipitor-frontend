029e7d354c37af22c42524e80ed3385cacd4af98
Ran Tavory <rantav@gmail.com>
2011-02-22 12:10:40 +0200

    Add pagimation and theming to the users admin pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users =  User.paginate :page => params[:page], :per_page => 5
 
     respond_to do |format|
       format.html # index.html.erb
