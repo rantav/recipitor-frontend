45b9320aac0644ed9684fff1c0862cbcd1885efa
Ran Tavory <rantav@gmail.com>
2011-02-23 01:36:36 +0200

    Start working on user autorization. Protect the admin pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :receipts
+
+  attr_accessible :admin
+
+  def admin?
+    admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110220133134) do
+ActiveRecord::Schema.define(:version => 20110222225440) do
 
   create_table "receipts", :force => true do |t|
     t.datetime "created_at"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20110220133134) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
