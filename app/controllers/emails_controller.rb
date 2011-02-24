5ef54570b86f1df25edcd4928f078860acbf2f63
Yonatan <yonatanm@gmail.com>
2011-02-24 10:02:50 +0200

    using loggign debug instead of puts, fixing the api key confition, rename uml file

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,39 +1,18 @@
 class EmailsController < ApplicationController
-
-  # POST /receipts
-  # POST /receipts.xml
-  def create_old
-  	puts "***************EmailsController::create creating new receipt from email #{params[:a]} __"  
-  	params.keys.each { |k| puts "[#{k}]==>[#{params[k]}]" }
-    @receipt = Receipt.new(params[:receipt])
-
-	puts "@receipt.save == #{@receipt.save} "
-    respond_to do |format|
-      if @receipt.save
-        format.html { redirect_to(@receipt, :notice => 'Receipt was successfully created.') }
-        format.xml  { render :xml => @receipt, :status => :created, :location => @receipt }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @receipt.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-  
   
    # POST /receipts
   # POST /receipts.xml
   def create
-  	puts "EmailsController::create"
-  	params.keys.each { |k| puts "[#{k}]==>[#{params[k]}]" if (k != :img )  }
-  	apikey = ERB.new(YAML.load_file("#{RAILS_ROOT}/config/upload.yml")[RAILS_ENV]["apikey"]).result
-  	puts "validating secret with #{apikey}"
-	if params["secret"].nil?
-		puts "secret does not exists"
-	elseif !(params["secret"].eql?(apikey))
-		puts "secret is incorrct"
+  	logger.debug "The object is EmailsController::create"
+  	params.keys.each { |k| logger.debug "[#{k}]==>[#{params[k]}]" if (k != :img )  }
+  	apikey = ERB.new(YAML.load_file("#{RAILS_ROOT}/config/api.yml")[RAILS_ENV]["apikey"]).result
+  	logger.debug  "validating [#{params['apikey']}] with [#{apikey}]"
+	if params["apikey"].nil? || !(params["apikey"].eql?(apikey))
+		logger.debug "apikey is incorrct or missing"
 	else
+		logger.debug "apikey matches"
 		@user = User.find_by_email(params[:user_email])
-		puts "looking for user with email #{params[:user_email]} found user \n #{@user}"
+		logger.debug "looking for user with email #{params[:user_email]} found user \n #{@user}"
 		if (@user.nil?)
 			return 
 		end
