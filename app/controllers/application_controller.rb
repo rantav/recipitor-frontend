56952614fb1368c246b8902edec200d6ca8a5592
Ran Tavory <rantav@gmail.com>
2011-03-10 15:56:36 +0200

    Force SSL in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :redirect_to_ssl
+
+  def redirect_to_ssl
+    redirect_to :protocol => "https://" unless (request.ssl? or !env_production?)
+  end
+
+  def env_production?
+    ENV['RAILS_ENV'] == "production"
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
