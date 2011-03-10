dec6da82e72ec512a07a4a8c3b750c7da99b4ea3
Ran Tavory <rantav@gmail.com>
2011-03-10 09:28:57 +0200

    Make password length up to 200 chars

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -74,7 +74,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length
-  # config.password_length = 6..20
+  config.password_length = 6..200
 
   # Regex to use to validate the email address
   # config.email_regexp = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
