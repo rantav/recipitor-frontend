de225e10a6e9927262c4fbcd0f4d01dafe8f1a19
Ran Tavory <rantav@gmail.com>
2011-03-31 09:52:09 +0200

    Make this a one liner

--- addNotableGroceryStores.rb
+++ addNotableGroceryStores.rb
@@ -1,13 +1,7 @@
-#require 'net/http'
+# To run this:
+# $ heroku console.
+# >> Paste this long line
+# condensed in one line for heroku's console; yuk
+g = JSON.parse(IO.read('NotableGroceryStores.json')); g['map'].each { |k,v|;  puts k; r = Net::HTTP.get 'api.duckduckgo.com', "/?q=#{URI.escape(k)}&o=json"; j = JSON.parse(r); url = nil; url =  j['Results'][0]['FirstURL'] if  j and j['Results'] and j['Results'][0]; StoreChain.create(:name => k, :url => url) and puts url}
 
-g = JSON.parse(IO.read('NotableGroceryStores.json'))
-
-g['map'].each do |k,v|
-  puts k
-  r = Net::HTTP.get 'api.duckduckgo.com', "/?q=#{URI.escape(k)}&o=json"
-  j = JSON.parse(r)
-  url = nil
-  url =  j['Results'][0]['FirstURL'] if  j and j['Results'] and j['Results'][0]
-  StoreChain.create(:name => k, :url => url) and puts url
-end
 
